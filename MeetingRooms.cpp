9961a4062b02c81b9bb6f4a67b5b7cd8c791cb34
Smitkumar Contractor <smitcontractor@gmail.com>
2023-08-05 10:29:43 -0700

    Added canAttendMeetings
    Added solution for Can Attend Meetings problem.

---
+++ MeetingRooms.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canAttendMeetings(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        vector<int> prev = intervals[0];
+        vector<int> cur;
+        for (auto it = ++intervals.begin(); it != intervals.end(); it++)
+        {
+            cur = *it;
+            if (prev[1] > cur[0])
+                return false;
+            prev = cur;
+        }
+        return true;
+    }
+};
