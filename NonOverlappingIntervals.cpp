d9af242e5e0038f3183d22f7e986c6447c358da1
Smitkumar Contractor <smitcontractor@gmail.com>
2023-08-05 10:22:21 -0700

    Added eraseOverlapIntervals
    Added solution for Erase Overlap Intervals problem.

---
+++ NonOverlappingIntervals.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        int non_overlapping = 0;
+        sort(intervals.begin(), intervals.end());
+        vector<int> prev = intervals[0];
+        vector<int> cur;
+        for (auto it = ++intervals.begin(); it != intervals.end(); it++)
+        {
+            cur = *it;
+            if (prev[1] > cur[0])
+            {
+                cur = { prev[0], min(prev[1], cur[1]) };
+                non_overlapping++;
+            }
+            
+            prev = cur;
+        }
+        return non_overlapping;
+    }
+};
