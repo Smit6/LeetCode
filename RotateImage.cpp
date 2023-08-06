67ddc241aea318a7d97b0ba798f8b43904504aca
Smitkumar Contractor <smitcontractor@gmail.com>
2023-08-06 10:44:19 -0700

    Solved Rotate Image
    Added solution for Rotate Image.

---
+++ RotateImage.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int up = 0, left = 0;
+        int down = matrix.size() - 1, right = matrix[0].size() - 1;
+        int up_left = 0;
+
+        while (up < down) {
+            for (int i = 0; i < right - left; i++) {
+                up_left = matrix[up][left + i];
+                matrix[up][left + i] = matrix[down - i][left];
+                matrix[down - i][left] = matrix[down][right - i];
+                matrix[down][right - i] = matrix[up + i][right];
+                matrix[up + i][right] = up_left;
+            }
+            ++up, --down, ++left, --right;
+        }
+    }
+};
