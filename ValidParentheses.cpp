552da27aba3a0d6169bfb057e61e5bb52c91dd15
Smitkumar Contractor <smitcontractor@gmail.com>
2023-08-10 12:08:59 -0700

    Solved SplitArrayLargestSum and Updated Valid Parentheses
    Added solution for SplitArrayLargestSum and Updated Valid Parentheses

--- ValidParentheses.cpp
+++ ValidParentheses.cpp
@@ -1,26 +1,31 @@
+// Time: O(N)
+// Space: O(N)
+
 class Solution
 {
 public:
-    bool isValid(string s)
-    {
-        unordered_map<char, char> close_open_bracket;
-        close_open_bracket[')'] = '(';
-        close_open_bracket['}'] = '{';
-        close_open_bracket[']'] = '[';
-
-        stack<char> st;
-
-        for (const auto& c : s)
-            if (!st.empty() && (close_open_bracket.find(c) != close_open_bracket.end()))
-            {
-                if (close_open_bracket[c] == st.top())
-                    st.pop();
-                else
-                    return false;
-            }
-            else
-                st.push(c);
-        
-        return st.empty() ? true : false;
-    }
+    bool isValid(string);
 };
+
+bool Solution::isValid(string s)
+{
+    unordered_map<char, char> close_open_bracket;
+    close_open_bracket[')'] = '(';
+    close_open_bracket['}'] = '{';
+    close_open_bracket[']'] = '[';
+
+    stack<char> st;
+
+    for (const auto& c : s)
+        if (!st.empty() && (close_open_bracket.find(c) != close_open_bracket.end()))
+        {
+            if (close_open_bracket[c] == st.top())
+                st.pop();
+            else
+                return false;
+        }
+        else
+            st.push(c);
+
+    return st.empty() ? true : false;
+}
