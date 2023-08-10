1cb86aa71e94a3f5889b3c07aba77406865cdfe3
Smitkumar Contractor <smitcontractor@gmail.com>
2023-08-10 10:24:30 -0700

    Updated Word Search
    Updated Word Search solution to extract function definitions from class.

--- WordSearch.cpp
+++ WordSearch.cpp
@@ -1,43 +1,48 @@
-class Solution
-{
+class Solution {
 public:
-    bool exist(vector<vector<char>>& board, string word)
-    {
-        rows = board.size(), cols = board[0].size();
-        word_size = word.size();
-        
-        for (int r = 0; r < rows; r++)
-            for (int c = 0; c < cols; c++)
-                if (dfs(board, word, 0, r, c))
-                    return true;
-
-        return false;
-    }
+    bool exist(vector<vector<char>>&, string);
 private:
-    int rows, cols;
-    int word_size = 0;
-    set<pair<int, int>> cycle;
+    int rows;
+    int cols;
+    unsigned int word_length;
+    set<pair<int, int>> visited;
 
-    bool dfs(vector<vector<char>>& board, string word, int i, int r, int c)
-    {
-        pair<int, int> rc(r, c);
-        if (cycle.find(rc) != cycle.end() || board[r][c] != word[i])
-            return false;
-        if (i == word_size - 1)
-            return true;
-        cycle.insert(rc);
-
-        if (0 <= r - 1 && dfs(board, word, i + 1, r - 1, c))
-            return true;
-        if (r + 1 < rows && dfs(board, word, i + 1, r + 1, c))
-            return true;
-        if (0 <= c - 1 && dfs(board, word, i + 1, r, c - 1))
-            return true;
-        if (c + 1 < cols && dfs(board, word, i + 1, r, c + 1))
-            return true;
-        
-        cycle.erase(rc);
-
-        return false;
-    }
+    bool dfs(vector<vector<char>>&, int, int, const string&, int);
 };
+
+
+bool Solution::exist(vector<vector<char>>& board, string word)
+{
+    rows = board.size();
+    cols = board[0].size();
+    word_length = word.length();
+
+    for (int r = 0; r < rows; r++)
+        for (int c = 0; c < cols; c++)
+            if (dfs(board, r, c, word, 0))
+                return true;
+
+    return false;
+}
+
+bool Solution::dfs(vector<vector<char>>& board, int r, int c, const string& word, int word_index)
+{
+    pair<int, int> rc(r, c);
+
+    if (visited.find(rc) != visited.end() || word[word_index] != board[r][c])
+        return false;
+
+    if (word_index == word_length - 1)
+        return true;
+
+    visited.insert(rc);
+
+    if (0 <= r - 1 && dfs(board, r - 1, c, word, word_index + 1)) return true;
+    if (r + 1 < rows && dfs(board, r + 1, c, word, word_index + 1)) return true;
+    if (0 <= c - 1 && dfs(board, r, c - 1, word, word_index + 1)) return true;
+    if (c + 1 < cols && dfs(board, r, c + 1, word, word_index + 1)) return true;
+
+    visited.erase(rc);
+
+    return false;
+}
