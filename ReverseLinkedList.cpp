5c783d29877958367aaaa6dadf4779575044d630
Smitkumar Contractor <smitcontractor@gmail.com>
2023-08-11 10:16:43 -0700

    Added time and space for Reverse Linked List
    Added time and space for Reverse Linked List problem. Also extracted function definition from class.

--- ReverseLinkedList.cpp
+++ ReverseLinkedList.cpp
@@ -1,19 +1,32 @@
-class Solution
-{
+// Time: O(N)
+// Space: O(1)
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
 public:
-    ListNode* reverseList(ListNode* head)
-    {
-        ListNode* prev = nullptr;
-        ListNode* cur = head;
-        ListNode* tmp = nullptr;
-        while (cur)
-        {
-            //prev, cur, cur.next = cur, cur.next, prev;
-            tmp = cur;
-            cur = cur->next;
-            tmp->next = prev;
-            prev = tmp;
-        }
-        return prev;
-    }
+    ListNode* reverseList(ListNode*);
 };
+
+ListNode* Solution::reverseList(ListNode* head) {
+    ListNode* prev = nullptr;
+    ListNode* cur = head;
+    ListNode* tmp = nullptr;
+
+    while (cur) {
+        tmp = cur;
+        cur = cur->next;
+        tmp->next = prev;
+        prev = tmp;
+    }
+
+    return prev;
+}
