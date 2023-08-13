75d4e6cf03ca1a293aab5673c66166c1e114d589
Smitkumar Contractor <smitcontractor@gmail.com>
2023-08-13 10:02:34 -0700

    Solved Longest Palindromic Substring
    Added solution for Longest Palindromic Substring.

--- LongestPalindromicSubstring.cpp
+++ LongestPalindromicSubstring.cpp
@@ -1,46 +1,44 @@
-class Solution
-{
+// Time: O(N ^ 2)
+// Space : O(1)
+
+class Solution {
 public:
-    string longestPalindrome(string s)
-    {
-        int s_length = s.length();
-        int left = 0, right = 0;
-        int longest_length = 0, current_length = 0;
-        pair<int, int> longest_palindrome_idx(0, 0);
+    string longestPalindrome(string);
+};
 
-        for (int i = 0; i < s_length; i++)
-        {
-            // ODD
-            left = i, right = i;
-            current_length = 0;
-            while (0 <= left && right < s_length && s[left] == s[right])
-            {
-                current_length = right - left + 1;
-                if (current_length > longest_length) {
-                    longest_length = current_length;
-                    longest_palindrome_idx.first = left;
-                    longest_palindrome_idx.second = right;
-                }
-                --left, ++right;
-            }
+string Solution::longestPalindrome(string s) {
+    int s_length = s.length();
+    int left = 0;
+    int right = 0;
+    int longest_length = 0;
+    int current_length = 0;
+    pair<int, int> longest_palindrome_index(0, 0);
 
-            // EVEN
-            left = i, right = i + 1;
-            current_length = 0;
-            while (0 <= left && right < s_length && s[left] == s[right])
-            {
-                current_length = right - left + 1;
-                if (current_length > longest_length) {
-                    longest_length = current_length;
-                    longest_palindrome_idx.first = left;
-                    longest_palindrome_idx.second = right;
-                }
-                --left, ++right;
+    for (int i = 0; i < s_length; i++) {
+        // ODD
+        left = i, right = i;
+        while (0 <= left && right < s_length && s[left] == s[right]) {
+            current_length = right - left + 1;
+            if (current_length > longest_length) {
+                longest_length = current_length;
+                longest_palindrome_index.first = left;
+                longest_palindrome_index.second = right;
             }
+            --left, ++right;
         }
 
-        cout << longest_palindrome_idx.first << ", " << longest_palindrome_idx.second;
-
-        return s.substr(longest_palindrome_idx.first, longest_palindrome_idx.second - longest_palindrome_idx.first + 1);
+        // EVEN
+        left = i, right = i + 1;
+        while (0 <= left && right < s_length && s[left] == s[right]) {
+            current_length = right - left + 1;
+            if (current_length > longest_length) {
+                longest_length = current_length;
+                longest_palindrome_index.first = left;
+                longest_palindrome_index.second = right;
+            }
+            --left, ++right;
+        }
     }
-};
+
+    return s.substr(longest_palindrome_index.first, longest_palindrome_index.second - longest_palindrome_index.first + 1);
+}
