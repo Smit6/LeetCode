ee7901fb11089bf0757d363bc9ada84a21bfccfc
Smitkumar Contractor <smitcontractor@gmail.com>
2023-08-14 09:50:16 -0700

    Extracted function definition in Merge Two Sorted Lists
    Extracted function definition in Merge Two Sorted Lists

--- MergeTwoSortedLists.cpp
+++ MergeTwoSortedLists.cpp
@@ -1,26 +1,34 @@
-class Solution
-{
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2)
-    {
-        ListNode* dummy = new ListNode();
-        ListNode* tmp = dummy;
-
-        while (list1 || list2)
-        {
-            if (list1 && (!list2 || list1->val <= list2->val))
-            {
-                tmp->next = list1;
-                list1 = list1->next;
-            }
-            else
-            {
-                tmp->next = list2;
-                list2 = list2->next;
-            }
-            tmp = tmp->next;
-        }
-
-        return dummy->next;
-    }
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2);
 };
+
+ListNode* Solution::mergeTwoLists(ListNode* list1, ListNode* list2) {
+    ListNode* dummy = new ListNode(0);
+    ListNode* tmp = dummy;
+
+    while (list1 || list2) {
+        if (list1 && (!list2 || list1->val <= list2->val)) {
+            tmp->next = list1;
+            list1 = list1->next;
+        }
+        else {
+            tmp->next = list2;
+            list2 = list2->next;
+        }
+        tmp = tmp->next;
+    }
+    ListNode* head = dummy->next;
+    delete dummy;
+    return head;
+}
