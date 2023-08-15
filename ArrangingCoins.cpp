4c510626dd8fe95f6c70b764f6561d39f94d6a17
Smitkumar Contractor <smitcontractor@gmail.com>
2023-08-15 12:08:23 -0700

    Solved Arranging Coins
    Added solution for Arranging Coins.

---
+++ ArrangingCoins.cpp
@@ -0,0 +1,27 @@
+// Time: O(Log N)
+// Space: O(1)
+
+class Solution {
+public:
+    int arrangeCoins(int);
+};
+
+int Solution::arrangeCoins(int n) {
+    int left = 1;
+    int right = n;
+    int res = 0;
+    int mid = 0;
+    int coins = 0;
+
+    while (left <= right) {
+        mid = (left + ((right - left) / 2));
+        coins = (mid * (mid + 1)) / 2;
+        if (coins > n)
+            right = mid - 1;
+        else if (coins < n)
+            left = mid + 1;
+        else
+            return mid;
+    }
+    return left - 1;
+}
