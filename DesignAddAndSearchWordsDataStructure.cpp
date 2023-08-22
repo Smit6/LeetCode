3be4cfebd19e082ed5fb46751e4b4a831b3ce72b
Smitkumar Contractor <smitcontractor@gmail.com>
2023-08-22 11:49:45 -0700

    Update DesignAddAndSearchWordsDataStructure.cpp

--- DesignAddAndSearchWordsDataStructure.cpp
+++ DesignAddAndSearchWordsDataStructure.cpp
@@ -1,61 +1,59 @@
-struct TrieNode
-{
+// Time: O(M) for well defined words, O(N*(26^M)) for undefined words
+// Space: O(1) for well defined words, O(M) for undefined words (to keep recursion stack)
+
+struct TrieNode {
     unordered_map<char, TrieNode*> children;
     bool is_end_of_word = false;
 };
 
-class WordDictionary
-{
+class WordDictionary {
 public:
-    WordDictionary()
-    {
-        root = new TrieNode();
-    }
-
-    void addWord(string word)
-    {
-        TrieNode* cur = root;
-        for (auto& c : word)
-        {
-            if (cur->children.find(c) == cur->children.end())
-                cur->children[c] = new TrieNode();
-            cur = cur->children[c];
-        }
-        cur->is_end_of_word = true;
-    }
-
-    bool search(string word)
-    {
-        return dfs(root, 0, word);
-    }
+    WordDictionary();
+    void addWord(string);
+    bool search(string);
 private:
     TrieNode* root;
-
-    bool dfs(TrieNode* node, int j, const string& word)
-    {
-        TrieNode* cur = node;
-
-        for (int i = j; i < word.length(); i++)
-        {
-            char c = word[i];
-            if (c == '.')
-            {
-                for (auto k_v : cur->children)
-                    if (dfs(k_v.second, i + 1, word))
-                        return true;
-                return false;
-            }
-            else
-            {
-                if (cur->children.find(c) == cur->children.end())
-                    return false;
-                cur = cur->children[c];
-            }
-        }
-
-        return cur->is_end_of_word;
-    }
+    bool dfs(TrieNode*, int, const string&);
 };
+
+
+WordDictionary::WordDictionary() {
+    root = new TrieNode();
+}
+
+void WordDictionary::addWord(string word) {
+    TrieNode* cur = root;
+    for (auto c : word) {
+        if (cur->children.find(c) == cur->children.end())
+            cur->children[c] = new TrieNode();
+        cur = cur->children[c];
+    }
+    cur->is_end_of_word = true;
+}
+
+bool WordDictionary::dfs(TrieNode* node, int j, const string& word) {
+    TrieNode* cur = node;
+    
+    for (int i = j; i < word.length(); i++) {
+        char c = word[i];
+        if (c == '.') {
+            for (auto& [k_c, v_node] : cur->children)
+                if (dfs(v_node, i + 1, word))
+                    return true;
+            return false;
+        } else {
+            if (cur->children.find(c) == cur->children.end())
+                return false;
+            cur = cur->children[c];
+        }
+    }
+
+    return cur->is_end_of_word;
+}
+
+bool WordDictionary::search(string word) {
+    return dfs(root, 0, word);
+}
 
 /**
  * Your WordDictionary object will be instantiated and called as such:
