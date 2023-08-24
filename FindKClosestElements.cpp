f1d949e7de5cfce18e90d9d27a87353d20e26dc9
Smitkumar Contractor <smitcontractor@gmail.com>
2023-08-24 08:06:42 -0700

    Solved Find K Closest Elements

--- FindKClosestElements.cpp
+++ FindKClosestElements.cpp
@@ -1,10 +1,22 @@
+// Time: O((logN) + K)
+// Space: O(1)
+
 class Solution {
 public:
-    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        
-    }
+    vector<int> findClosestElements(vector<int>&, int, int);
 };
 
-vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        
+vector<int> Solution::findClosestElements(vector<int>& arr, int k, int x) {
+    int left = 0;
+    int right = arr.size() - k;
+
+    while (left < right) {
+        int mid = (left + ((right - left) / 2));
+        if (x - arr[mid] > arr[mid + k] - x)
+            left = mid + 1;
+        else
+            right = mid - 1;
     }
+
+    return {arr.begin() + left, arr.begin() + left + k};
+}
