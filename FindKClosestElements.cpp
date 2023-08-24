955b7415b4839f91c71ac57f046de99598f3b0f4
Smitkumar Contractor <smitcontractor@gmail.com>
2023-08-24 07:52:01 -0700

    Extracted function definitions and added new file

---
+++ FindKClosestElements.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        
+    }
+};
+
+vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        
+    }
--- SwapNodesInPairs.cpp
+++ SwapNodesInPairs.cpp
@@ -13,28 +13,30 @@
  */
 class Solution {
 public:
-    ListNode* swapPairs(ListNode* head) {
-        ListNode* dummy = new ListNode(0, head);
-        ListNode* prev = dummy;
-        ListNode* tmp = head;
-
-        ListNode* p1;
-        ListNode* p2;
-        ListNode* nxt;
-
-        while (tmp && tmp->next) {
-            p1 = tmp;
-            p2 = tmp->next;
-            nxt = tmp->next->next;
-
-            p1->next = nxt;
-            p2->next = p1;
-            prev->next = p2;
-
-            prev = p1;
-            tmp = nxt;
-        }
-
-        return dummy->next;
-    }
+    ListNode* swapPairs(ListNode*);
 };
+
+ListNode* Solution::swapPairs(ListNode* head) {
+    ListNode* dummy = new ListNode(0, head);
+    ListNode* prev = dummy;
+    ListNode* tmp = head;
+
+    ListNode* p1;
+    ListNode* p2;
+    ListNode* nxt;
+
+    while (tmp && tmp->next) {
+        p1 = tmp;
+        p2 = tmp->next;
+        nxt = tmp->next->next;
+
+        p1->next = nxt;
+        p2->next = p1;
+        prev->next = p2;
+
+        prev = p1;
+        tmp = nxt;
+    }
+
+    return dummy->next;
+}
