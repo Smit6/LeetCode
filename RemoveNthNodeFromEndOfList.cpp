cd5e9804c16e25041364a2a7099a56a1761b8fdf
Smitkumar Contractor <smitcontractor@gmail.com>
2023-08-24 07:30:19 -0700

    Added time and space for Remove Nth Node From End of List
    Added time and space for Remove Nth Node From End of List

--- RemoveNthNodeFromEndOfList.cpp
+++ RemoveNthNodeFromEndOfList.cpp
@@ -1,25 +1,41 @@
-class Solution
-{
+// Time: O(N)
+// Space: O(1)
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
 public:
-    ListNode* removeNthFromEnd(ListNode* head, int n)
-    {
-        ListNode* dummy = new ListNode(0, head);
-        ListNode* slow = dummy;
-        ListNode* fast = head;
-
-        while (n-- > 0 && fast)
-            fast = fast->next;
-
-        while (fast)
-        {
-            slow = slow->next;
-            fast = fast->next;
-        }
-
-        ListNode* tmp = slow->next;
-        slow->next = slow->next->next;
-        delete tmp;
-
-        return dummy->next;
-    }
+    ListNode* removeNthFromEnd(ListNode*, int);
 };
+
+ListNode* Solution::removeNthFromEnd(ListNode* head, int n) {
+    ListNode* dummy = new ListNode(0, head);
+    ListNode* slow = dummy;
+    ListNode* fast = dummy;
+
+    while (n-- && fast->next)
+        fast = fast->next;
+    
+    while (fast->next) {
+        slow = slow->next;
+        fast = fast->next;
+    }
+
+    ListNode* to_delete = slow->next;
+    slow->next = slow->next->next;
+    delete to_delete;
+
+    to_delete = dummy;
+    dummy = dummy->next;
+    delete to_delete;
+
+    return dummy;
+}
