19885062256c28caab10adf32cdce40e806c19d1
Smitkumar Contractor <smitcontractor@gmail.com>
2023-08-26 04:46:01 -0700

    Added time and space for LongestSubstringWithoutRepeatingCharacters

--- LongestSubstringWithoutRepeatingCharacters.cpp
+++ LongestSubstringWithoutRepeatingCharacters.cpp
@@ -1,23 +1,24 @@
-class Solution
-{
+// Time: O(N)
+// Space: O(1)
+
+class Solution {
 public:
-    int lengthOfLongestSubstring(string s)
-    {
-        unordered_map<char, int> ch_map;
-        int s_size = s.length();
-        int cur_substr_index = 0;
-        int max_length = 0;
-
-        //a b c a b c b b
-        for (int i = 0; i < s_size; i++)
-        {
-            if (ch_map.find(s[i]) != ch_map.end() && ch_map[s[i]] >= cur_substr_index)
-                cur_substr_index = ch_map[s[i]] + 1;
-            else
-                max_length = max(max_length, i - cur_substr_index + 1);
-            ch_map[s[i]] = i;
-        }
-
-        return max_length;
-    }
+    int lengthOfLongestSubstring(string);
 };
+
+int Solution::lengthOfLongestSubstring(string s) {
+    unordered_map<char, int> char_index;
+    int longest{};
+    int l{};
+    
+    for (int r{0}; r < s.length(); r++) {
+        if (char_index.contains(s[r]) && char_index[s[r]] >= l) {
+            l = char_index[s[r]] + 1;
+        } else {
+            longest = max(longest, r - l + 1);
+        }
+        char_index[s[r]] = r;
+    }
+
+    return longest;
+}
