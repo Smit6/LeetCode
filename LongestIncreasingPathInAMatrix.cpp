a12e48519955693516179f5a64ee42de9a0d05a9
Smitkumar Contractor <smitcontractor@gmail.com>
2023-08-27 08:36:50 -0700

    Update LongestIncreasingPathInAMatrix.cpp

--- LongestIncreasingPathInAMatrix.cpp
+++ LongestIncreasingPathInAMatrix.cpp
@@ -5,33 +5,29 @@ class Solution {
 public:
     int longestIncreasingPath(vector<vector<int>>&);
 private:
-    int rows = 0;
-    int cols = 0;
+    int rows{0};
+    int cols{0};
     map<pair<int, int>, int> cache;
-
-    int dfs(vector<vector<int>>&, int, int, int);
+    int dfs(const vector<vector<int>>&, int, int, int);
 };
 
-int Solution::dfs(vector<vector<int>>& matrix, int r, int c, int prev) {    
+int Solution::dfs(const vector<vector<int>>& matrix, int r, int c, int prev) {
     if (prev >= matrix[r][c])
         return 0;
-    
     auto rc = make_pair(r, c);
-    
-    if (cache.find(rc) != cache.end())
+    if (cache.contains(rc))
         return cache[rc];
-
-    int rc_longest = 0;
-
+    
+    int rc_longest{0};
     if (0 <= r - 1)
-        rc_longest = max(dfs(matrix, r - 1, c, matrix[r][c]), rc_longest);
+        rc_longest = max(rc_longest, dfs(matrix, r - 1, c, matrix[r][c]));
     if (r + 1 < rows)
-        rc_longest = max(dfs(matrix, r + 1, c, matrix[r][c]), rc_longest);
+        rc_longest = max(rc_longest, dfs(matrix, r + 1, c, matrix[r][c]));
     if (0 <= c - 1)
-        rc_longest = max(dfs(matrix, r, c - 1, matrix[r][c]), rc_longest);
+        rc_longest = max(rc_longest, dfs(matrix, r, c - 1, matrix[r][c]));
     if (c + 1 < cols)
-        rc_longest = max(dfs(matrix, r, c + 1, matrix[r][c]), rc_longest);
-
+        rc_longest = max(rc_longest, dfs(matrix, r, c + 1, matrix[r][c]));
+    
     cache[rc] = rc_longest + 1;
     return cache[rc];
 }
@@ -39,12 +35,11 @@ int Solution::dfs(vector<vector<int>>& matrix, int r, int c, int prev) {
 int Solution::longestIncreasingPath(vector<vector<int>>& matrix) {
     rows = matrix.size();
     cols = matrix[0].size();
-
-    int longest = 0;
+    int longest{0};
 
     for (int r = 0; r < rows; r++)
         for (int c = 0; c < cols; c++)
-            longest = max(dfs(matrix, r, c, -1), longest);
-    
+            longest = max(longest, dfs(matrix, r, c, -1));
+
     return longest;
 }
